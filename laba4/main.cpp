9459d2e32fcfaa452feba6841fea20c3e6f712d4
GRobik11 <grigoryanrob1@yandex.ru>
2024-05-24 17:50:08 +0200

    Добавлены лабы

---
+++ laba4/main.cpp
@@ -0,0 +1,69 @@
+﻿#include <SFML/Graphics.hpp>
+#include <iostream>
+
+
+
+int main() {
+    const int N = 5; // Задаем количество фигур
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Shapes"); // Создаем окно
+
+    // Создаем круги
+    sf::CircleShape circles[N];
+    for (int i = 0; i < N; i++) {
+        circles[i].setRadius(50.f);
+        circles[i].setFillColor(sf::Color::Green);
+        circles[i].setPosition(100.f * i, 100.f);
+    }
+
+    // Создаем треугольники
+    sf::ConvexShape triangles[N];
+    for (int i = 0; i < N; i++) {
+        triangles[i].setPointCount(3);
+        triangles[i].setPoint(0, sf::Vector2f(0.f, 0.f));
+        triangles[i].setPoint(1, sf::Vector2f(100.f, 0.f));
+        triangles[i].setPoint(2, sf::Vector2f(50.f, 100.f));
+        triangles[i].setFillColor(sf::Color::Blue);
+        triangles[i].setPosition(100.f * i, 250.f);
+    }
+
+    // Создаем прямоугольники
+    sf::RectangleShape rectangles[N];
+    for (int i = 0; i < N; i++) {
+        rectangles[i].setSize(sf::Vector2f(100.f, 50.f));
+        rectangles[i].setFillColor(sf::Color::Red);
+        rectangles[i].setPosition(100.f * i, 400.f);
+    }
+
+    // Создаем линии
+    sf::VertexArray lines(sf::Lines, N * 2);
+    for (int i = 0; i < N * 2; i += 2) {
+        lines[i].position = sf::Vector2f(500.f, 50.f * i);
+        lines[i].color = sf::Color::Yellow;
+        lines[i + 1].position = sf::Vector2f(700.f, 50.f * i);
+        lines[i + 1].color = sf::Color::Yellow;
+    }
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear();
+
+        // Отображаем круги, треугольники, прямоугольники и линии
+        for (int i = 0; i < N; i++) {
+            window.draw(circles[i]);
+            window.draw(triangles[i]);
+            window.draw(rectangles[i]);
+        }
+        window.draw(lines);
+
+        window.display();
+    }
+
+    return 0;
+}
---
+++ laba6/main.cpp
@@ -0,0 +1,53 @@
+﻿#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML Image");
+    sf::Texture backgroundTexture;
+    if (!backgroundTexture.loadFromFile("1.jpg"))
+    {
+        return -1; 
+    }
+    sf::Sprite background(backgroundTexture);
+
+    sf::Texture spaceshipTexture;
+    if (!spaceshipTexture.loadFromFile("2.jpg"))
+    {
+        return -1; 
+    }
+    sf::Sprite spaceship(spaceshipTexture);
+
+    background.setPosition(0, 0);
+    spaceship.setPosition(400, 300);
+
+    while (window.isOpen())
+    {
+
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+        sf::Vector2f movement(0.f, 0.f);
+        float speed = 1/4.f;
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+            movement.y -= speed;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+            movement.y += speed;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+            movement.x -= speed;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+            movement.x += speed;
+
+        spaceship.move(movement);
+        window.clear();
+        window.draw(background);
+        window.draw(spaceship);
+        window.display();
+    }
+    return 0;
+}
